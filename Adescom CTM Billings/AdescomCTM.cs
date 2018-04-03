53ae6222458ccfd2d41853044aa8f949d9b6ea8d
Baroo <baroo.phone@live.com>
2018-04-03 18:21:20 +0200

    Added AdescomCTM classes with SOAP wrapper

---
+++ Adescom CTM Billings/AdescomCTM.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using AdescomWebService;
+using Microsoft.Extensions.Configuration;
+using Adescom_CTM_Billings.Models;
+
+namespace Adescom_CTM_Billings
+{
+    class AdescomCTM
+    {
+        public IConfiguration _configuration { get; set; }
+
+        public AdescomCTM(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<List<Client>> GetClientsAsyncWithTimeout(bool includeCLIDs, int? clientId)
+        {
+            try
+            {
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                Task timeoutTask = Task.Delay(TimeSpan.FromSeconds(_configuration.GetValue<int>("WebServiceConnectionSettings:RequestTimeout")));
+
+                Task<List<Client>> task;
+
+                if (await Task.WhenAny(task = GetClientsAsync(includeCLIDs, clientId), timeoutTask) == timeoutTask)
+                {
+                    cancellationTokenSource.Cancel();
+                    throw new TimeoutException();
+                }
+
+                return await task;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<Client>> GetClientsAsync(bool includeCLIDs, int? clientId)
+        {
+            try
+            {
+                string sessionId = "";
+                AdescomCTMSoapWrapper atmanCTMWithoutProxyClassesClient = new AdescomCTMSoapWrapper(_configuration);
+                ConcurrentBag<Client> clients = new ConcurrentBag<Client>();
+                sessionId = await atmanCTMWithoutProxyClassesClient.LoginAsync(
+                    _configuration.GetValue<string>("WebServiceConnectionSettings:ResellerLogin"), 
+                    _configuration.GetValue<string>("WebServiceConnectionSettings:ResellerPassword"), 
+                    _configuration.GetValue<int>("WebServiceConnectionSettings:SessionTimeout"));
+                clientDetailsArray clientDetailsArray = await atmanCTMWithoutProxyClassesClient.GetClientsAsync();
+
+                List<clientDetails> clientDetailsFiltered;
+
+                if (clientId.HasValue)
+                    clientDetailsFiltered = clientDetailsArray.clients.Where(c => c.clientID == clientId.Value).ToList();
+                else
+                    clientDetailsFiltered = clientDetailsArray.clients.ToList();
+
+                if (includeCLIDs)
+                {
+                    if (clientDetailsArray != null && clientDetailsArray.count > 0)
+                    {
+                        Parallel.ForEach(clientDetailsFiltered, new ParallelOptions { MaxDegreeOfParallelism = 1 }, (client) =>
+                        {
+                            GetCLIDsForClient(client, atmanCTMWithoutProxyClassesClient, clients);
+                        });
+                    }
+                }
+                else
+                {
+                    foreach (clientDetails clientDetails in clientDetailsFiltered)
+                        clients.Add(new Client(clientDetails.clientID, clientDetails.billingDetails.name, new List<Clid>() { }));
+                }
+
+                await atmanCTMWithoutProxyClassesClient.LogoutAsync(sessionId);
+                return clients.ToList();
+            }
+            catch (AggregateException ex)
+            {
+                throw new Exception(ex.InnerException.Message);
+            }
+        }
+
+
+        private async void GetCLIDsForClient(clientDetails client, AdescomCTMSoapWrapper atmanCTMWithoutProxyClassesClient, ConcurrentBag<Client> clients)
+        {
+            List<Clid> clids = new List<Clid>();
+
+            // Get CLIDs
+            clidDetailsArray clidDetailsArray = atmanCTMWithoutProxyClassesClient.GetCLIDsForClientAsync(client.clientID).Result;
+            if (clidDetailsArray != null && clidDetailsArray.count > 0)
+                foreach (var clid in clidDetailsArray.clids)
+                    if (clid.active)
+                        clids.Add(new Clid(clid.number.countryCode, clid.number.areaCode, clid.number.shortCLID));
+
+            // Get trunk groups CLIDs
+            TrunkgroupsArray trunkgroupsArray = atmanCTMWithoutProxyClassesClient.GetTrunkGroupsForClientAsync(client.clientID).Result;
+            if (trunkgroupsArray != null && trunkgroupsArray.count > 0)
+                foreach (var trunk in trunkgroupsArray.items)
+                    if (trunk.active)
+                        foreach (var clid in trunk.numbers.items)
+                            clids.Add(new Clid(clid.countryCode, clid.areaCode, clid.shortCLID));
+
+            clients.Add(new Client(client.clientID, client.billingDetails.name, clids.Distinct().ToList()));
+        }
+
+        public async Task<List<ClientBilling>> GetBillingForClientsAsyncWithTimeout(List<Client> clients, DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                Task timeoutTask = Task.Delay(TimeSpan.FromSeconds(_configuration.GetValue<int>("WebServiceConnectionSettings:RequestTimeout")));
+
+                Task<List<ClientBilling>> task;
+
+                if (await Task.WhenAny(task = GetBillingForClientsAsync(clients, startDate, endDate), timeoutTask) == timeoutTask)
+                {
+                    cancellationTokenSource.Cancel();
+                    throw new TimeoutException();
+                }
+
+                return await task;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<ClientBilling>> GetBillingForClientsAsync(List<Client> clients, DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                string sessionId = "";
+                AdescomCTMSoapWrapper atmanCTMWithoutProxyClassesClient = new AdescomCTMSoapWrapper(_configuration);
+                BillingQueryOptions billingQueryOptions = new BillingQueryOptions()
+                {
+                    incoming = false,
+                    outgoing = true,
+                    includeZeroDuration = false
+                };
+
+                ConcurrentBag<ClientBilling> clientsBillings = new ConcurrentBag<ClientBilling>();
+
+                sessionId = await atmanCTMWithoutProxyClassesClient.LoginAsync(
+                    _configuration.GetValue<string>("WebServiceConnectionSettings:ResellerLogin"), 
+                    _configuration.GetValue<string>("WebServiceConnectionSettings:ResellerPassword"), 
+                    _configuration.GetValue<int>("WebServiceConnectionSettings:SessionTimeout"));
+
+                Parallel.ForEach(clients, new ParallelOptions { MaxDegreeOfParallelism = 20 }, (client) =>
+                {
+                    GetBillingForClient(startDate, endDate, client, atmanCTMWithoutProxyClassesClient, billingQueryOptions, clientsBillings);
+                });
+
+                await atmanCTMWithoutProxyClassesClient.LogoutAsync(sessionId);
+                return clientsBillings.ToList();
+            }
+            catch (AggregateException ex)
+            {
+                throw new Exception(ex.InnerException.Message);
+            }
+        }
+
+        private async void GetBillingForClient(DateTime fromDate, DateTime toDate, Client client, AdescomCTMSoapWrapper atmanCTMWithoutProxyClassesClient, BillingQueryOptions billingQueryOptions, ConcurrentBag<ClientBilling> clientsBillings)
+        {
+            List<BillingRecord> billingRecords = new List<BillingRecord>();
+            BillingRecordsArrayEx billingRecordsArrayEx = atmanCTMWithoutProxyClassesClient.GetBillingForClientAsync(client.Id, fromDate, toDate).Result;
+
+            if (billingRecordsArrayEx != null && billingRecordsArrayEx.totalCount > 0)
+            {
+                foreach (BillingRecordEx billingRecordEx in billingRecordsArrayEx.items)
+                {
+                    double price = 0;
+                    double priceInclTaxes = 0;
+
+                    if (billingRecordEx.price.HasValue)
+                        price = billingRecordEx.price.Value;
+
+                    if (billingRecordEx.priceInclTaxes.HasValue)
+                        priceInclTaxes = billingRecordEx.priceInclTaxes.Value;
+
+                    billingRecords.Add(new BillingRecord(billingRecordEx.startDate, billingRecordEx.source, billingRecordEx.destination, billingRecordEx.duration, price));
+                }
+            }
+
+            // Billing records filtering by destination
+            List<string> destinationFilter = new List<string>() {
+                "SUBSCRIBE",
+                "CENTREX",
+                "HUNT-GROUP",
+                "CLIENT_SUBSCRIBE",
+                "CLIENT_TRUNK",
+                "CLIENT_TRUNK_GAP",
+                "CLIENT_TRUNK_SUBSCRIBE",
+                "CLID_CFU",
+                "CLID_CLIP",
+                "CLID_FORWARDING",
+                "CLID_ATXFERALLOWED",
+                "CLID_BLINDXFERALLOWED",
+                "RECORDINGS_STORAGE_UNLIMITED_MON",
+                "MASS_DIAL_CONNECTION_SLOT",
+                "CLIENT_SUBSCRIBE_SPECIAL",
+                "CLIENT_SUBSCRIBE_SPECIAL_1",
+                "CLIENT_SUBSCRIBE_SPECIAL_2",
+                "CLIENT_SUBSCRIBE_SPECIAL_3",
+                "CLIENT_SUBSCRIBE_SPECIAL_4",
+                "CLIENT_SUBSCRIBE_SPECIAL_5",
+                "CLIENT_SUBSCRIBE_SPECIAL_6",
+                "CLIENT_SUBSCRIBE_SPECIAL_7",
+                "CLIENT_SUBSCRIBE_SPECIAL_8",
+                "CLIENT_SUBSCRIBE_SPECIAL_9",
+                "CLIENT_SUBSCRIBE_SPECIAL_10"
+            };
+
+            clientsBillings.Add(new ClientBilling(client, fromDate, toDate, billingRecords.
+                Where(r => !((r.Price == 0) && (destinationFilter.Contains(r.Destination))))
+                .ToList()));
+        }
+    }
+}
---
+++ Adescom CTM Billings/AdescomCTMSoapWrapper.cs
@@ -0,0 +1,294 @@
+﻿using AdescomWebService;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Adescom_CTM_Billings
+{
+    class AdescomCTMSoapWrapper
+    {
+        public IConfiguration _configuration { get; set; }
+        HttpClient _httpClient;
+        Uri _webServiceUri;
+      
+        const string _loginRequest = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <env:Envelope xmlns:env=""http://www.w3.org/2003/05/soap-envelope"" xmlns:ns1=""http://www.example.org/ResellerAPI/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:enc=""http://www.w3.org/2003/05/soap-encoding"">
+                                <env:Body>
+                                    <ns1:login env:encodingStyle=""http://www.w3.org/2003/05/soap-encoding"">
+                                        <username xsi:type=""xsd:string"">%USERNAME%</username>
+                                        <password xsi:type=""xsd:string"">%PASSWORD%</password>
+                                        <timeout xsi:type=""xsd:int"">%TIMEOUT%</timeout>
+                                    </ns1:login>
+                                </env:Body>
+                            </env:Envelope>";
+
+
+        const string _clientsRequest = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"">
+	                            <s:Body s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+		                            <q1:getClients xmlns:q1=""http://www.example.org/ResellerAPI/"">
+			                            <params href=""#id1""/>
+		                            </q1:getClients>
+		                            <q2:clientSearchParams id=""id1"" xsi:type=""q2:clientSearchParams"" xmlns:q2=""http://www.example.org/ResellerAPI/"">
+			                            <resellerID xsi:type=""xsd:int"">0</resellerID>
+		                            </q2:clientSearchParams>
+	                            </s:Body>
+                            </s:Envelope>";
+
+        const string _CLIDsRequest = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"">
+                                <s:Body xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+                                    <q1:getCLIDsForClient xmlns:q1=""http://www.example.org/ResellerAPI/"">
+                                        <clientID xsi:type=""xsd:int"">%CLIENTID%</clientID>
+                                    </q1:getCLIDsForClient>
+                                </s:Body>
+                            </s:Envelope>";
+
+        const string _trunkGroupsRequest = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <env:Envelope xmlns:env=""http://www.w3.org/2003/05/soap-envelope"" xmlns:ns1=""http://www.example.org/ResellerAPI/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:enc=""http://www.w3.org/2003/05/soap-encoding"">
+                                <env:Body>
+                                    <ns1:getTrunkgroupsForClient env:encodingStyle=""http://www.w3.org/2003/05/soap-encoding"">
+                                        <clientID xsi:type=""xsd:int"">%CLIENTID%</clientID>
+                                    </ns1:getTrunkgroupsForClient>
+                                </env:Body>
+                            </env:Envelope>";
+
+        const string _billingRequest = @"<env:Envelope xmlns:env=""http://www.w3.org/2003/05/soap-envelope"" xmlns:ns1=""http://www.example.org/ResellerAPI/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:enc=""http://www.w3.org/2003/05/soap-encoding"">
+                                        <env:Body>
+                                            <ns1:getBillingForClient env:encodingStyle=""http://www.w3.org/2003/05/soap-encoding"">
+                                                <clientID xsi:type=""xsd:int"">%CLIENTID%</clientID>
+                                                <fromDate xsi:type=""xsd:dateTime"">%FROMDATE%</fromDate>
+                                                <toDate xsi:type=""xsd:dateTime"">%TODATE%</toDate>
+                                                <options xsi:type=""ns1:BillingQueryOptions"">
+                                                    <incoming xsi:type=""xsd:boolean"">false</incoming>
+                                                    <outgoing xsi:type=""xsd:boolean"">true</outgoing>
+                                                    <includeZeroDuration xsi:type=""xsd:boolean"">false</includeZeroDuration>
+                                                    <countCDRs xsi:type=""xsd:boolean"">true</countCDRs>
+                                                    <sumCDRs xsi:type=""xsd:boolean"">true</sumCDRs>
+                                                    <strict xsi:type=""xsd:boolean"">true</strict>
+                                                </options>
+                                            </ns1:getBillingForClient>
+                                        </env:Body>
+                                    </env:Envelope>";
+
+        const string logoutRequest = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <env:Envelope xmlns:env=""http://www.w3.org/2003/05/soap-envelope"" xmlns:ns1=""http://www.example.org/ResellerAPI/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:enc=""http://www.w3.org/2003/05/soap-encoding"">
+                                <env:Body>
+                                    <ns1:logout env:encodingStyle=""http://www.w3.org/2003/05/soap-encoding"">
+                                        <sessionID xsi:type=""xsd:string"">%SESSIONID%</sessionID>
+                                    </ns1:logout>
+                                </env:Body>
+                            </env:Envelope>";
+
+
+        public AdescomCTMSoapWrapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = new HttpClient();
+            _webServiceUri = new Uri(_configuration.GetValue<string>("WebServiceConnectionSettings:Url"));
+        }
+
+        public async Task<string> LoginAsync(string username, string password, int timeout)
+        {
+            try
+            {
+                return await GetSoapReponseForLogin(username, password, timeout);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<clientDetailsArray> GetClientsAsync()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(clientDetailsArray));
+            MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes((await GetSoapReponseForGetClientsAsync()).Replace("getClientsResponse", "clientDetailsArray")));
+            return xmlSerializer.Deserialize(memoryStream) as clientDetailsArray;
+        }
+
+        public async Task<clidDetailsArray> GetCLIDsForClientAsync(int clinetId)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(clidDetailsArray));
+                MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes((await GetSoapReponseForGetCLIDsForClientAsync(clinetId)).Replace("getCLIDsForClientResponse", "clidDetailsArray")));
+                return xmlSerializer.Deserialize(memoryStream) as clidDetailsArray;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<TrunkgroupsArray> GetTrunkGroupsForClientAsync(int clinetId)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(TrunkgroupsArray));
+                MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes((await GetSoapReponseForGetTrunkGroupsForClientAsync(clinetId)).Replace("getTrunkgroupsForClientResponse", "TrunkgroupsArray")));
+                return xmlSerializer.Deserialize(memoryStream) as TrunkgroupsArray;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<BillingRecordsArrayEx> GetBillingForClientAsync(int clientId, DateTime fromDate, DateTime toDate)
+        {
+            try
+            { 
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(BillingRecordsArrayEx));
+                var aa = (await GetSoapReponseForGetBillingForClientAsync(clientId, fromDate, toDate)).Replace("records", "BillingRecordsArrayEx");
+                MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(aa));
+                return xmlSerializer.Deserialize(memoryStream) as BillingRecordsArrayEx;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> LogoutAsync(string sessionID)
+        {
+            try
+            {
+                return await GetSoapReponseForLogoutAsync(sessionID);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForLogin(string username, string password, int timeout)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(_loginRequest.Replace("%USERNAME%", username.ToString()).Replace("%PASSWORD%", password.ToString()).Replace("%TIMEOUT%", timeout.ToString())));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                    .Descendants((XNamespace)"http://www.w3.org/2003/05/soap-envelope" + "Body")
+                    .First()
+                    .Descendants("loginResponse")
+                    .Single()
+                    .Value;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForGetClientsAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(_clientsRequest));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                    .Descendants((XNamespace)"http://schemas.xmlsoap.org/soap/envelope/" + "Body")
+                    .First()
+                    .DescendantNodes()
+                    .Skip(1)
+                    .First().ToString().Replace("ns1:", "");
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForGetCLIDsForClientAsync(int clientId)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(_CLIDsRequest.Replace("%CLIENTID%", clientId.ToString())));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                .Descendants((XNamespace)"http://schemas.xmlsoap.org/soap/envelope/" + "Body")
+                .First()
+                .DescendantNodes()
+                .Skip(1)
+                .First()
+                .ToString().Replace("ns1:", "");
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForGetTrunkGroupsForClientAsync(int clientId)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(_trunkGroupsRequest.Replace("%CLIENTID%", clientId.ToString())));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                    .Descendants((XNamespace)"http://www.w3.org/2003/05/soap-envelope" + "Body")
+                    .First()
+                    .DescendantNodes()
+                    .Skip(3)
+                    .First()
+                    .ToString().Replace("ns1:", "");
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForGetBillingForClientAsync(int clientId, DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(_billingRequest.Replace("%CLIENTID%", clientId.ToString()).Replace("%FROMDATE%", fromDate.ToString("o")).Replace("%TODATE%", toDate.ToString("o"))));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                    .Descendants((XNamespace)"http://schemas.xmlsoap.org/soap/envelope/" + "Body")
+                    .First()
+                    .DescendantNodes()
+                    .Skip(1)
+                    .First()
+                    .ToString().Replace("ns1:", "");    // Date format 2018-02-03T00:00:00.0000000+01:00
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<string> GetSoapReponseForLogoutAsync(string sessionID)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(_webServiceUri, new StringContent(logoutRequest.Replace("%SESSIONID%", sessionID)));
+                response.EnsureSuccessStatusCode();
+
+                return XDocument.Parse(await response.Content.ReadAsStringAsync())
+                    .Descendants((XNamespace)"http://www.w3.org/2003/05/soap-envelope" + "Body")
+                    .First()
+                    .Descendants("logoutResponse")
+                    .Single()
+                    .Value;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
