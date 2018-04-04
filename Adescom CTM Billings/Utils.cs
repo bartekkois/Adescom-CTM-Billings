bf456d9ed1078d4a93dc120f332d7e4c2babe41f
Baroo <baroo.phone@live.com>
2018-04-04 17:05:56 +0200

    Fixed RemoveInvalidCharactersFromFilename method to remove invalid characters from filenames for both Windows and Unix/Linux systems

--- Adescom CTM Billings/Utils.cs
+++ Adescom CTM Billings/Utils.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -9,8 +8,21 @@ namespace Adescom_CTM_Billings
     {
         public static string RemoveInvalidCharactersFromFilename(string inputFilename)
         {
-            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
-            Regex regex = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            List<char> _invalidFileNameChars = new List<char> {
+                '\x0022', '\x003C', '\x003E', '\x007C',
+                '\x0000', '\x0001', '\x0002', '\x0003',
+                '\x0004', '\x0005', '\x0006', '\x0007',
+                '\x0008', '\x0009', '\x000A', '\x000B',
+                '\x000C', '\x000D', '\x000E', '\x000F',
+                '\x0010', '\x0011', '\x0012', '\x0013',
+                '\x0014', '\x0015', '\x0016', '\x0017',
+                '\x0018', '\x0019', '\x001A', '\x001B',
+                '\x001C', '\x001D', '\x001E', '\x001F',
+                '\x003A', '\x002A', '\x003F', '\x005C',
+                '\x002F'
+            };
+
+            Regex regex = new Regex(string.Format("[{0}]", Regex.Escape(new string(_invalidFileNameChars.ToArray()))));
             return regex.Replace(inputFilename, "");
         }
 
