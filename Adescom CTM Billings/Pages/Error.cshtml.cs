03f658a3f3e29859b6e0a919f8d2e83ee7abd828
Baroo <baroo.phone@live.com>
2018-04-04 17:33:31 +0200

    Implemented basic UI for Index and Preview pages

--- Adescom CTM Billings/Pages/Error.cshtml.cs
+++ Adescom CTM Billings/Pages/Error.cshtml.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Adescom_CTM_Billings.Pages
+namespace WebApplication1.Pages
 {
     public class ErrorModel : PageModel
     {
